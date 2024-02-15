21983df153f2c82ecabba1f22a0d6c6513326ee3
sean <spmcdonald1316@gmail.com>
2024-02-15 13:18:52 -0600

    finish bulk renamer, update README

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ Created by Code With Tomi
 Hosted on freeCodeCamp Youtube channel  
   
 Project 1 ~ Web Scraper  
-  - build a web scraper to get the user photo from a github profile  ]
+  - build a web scraper to get the user photo from a github profile  
   - use requests and BeautifulSoup libraries  
   - return url of github avatar  
     
 Project 2 ~ Renaming Bulk Files  
   - script to rename all files in a directory  
-  - 
+  - takes user input for path, new name, and extension  
+  - using os library renames all files in directory
--- renaming_bulk_files/main.py
+++ renaming_bulk_files/main.py
@@ -3,17 +3,14 @@ import os
 def main():
   i = 1
   path = input('Enter directory path: ')
-  print(path)
   output_name = input('Enter new name for files: ')
-  print(output_name)
   output_file_ext = input('Enter output file extension: ')
-  print(output_file_ext)
 
-  # for filename in os.listdir(path):
-  #     output = f'{path}{output_name}{i}{output_file_ext}'
-  #     source = path + filename
-  #     os.rename(source, output)
-  #     i += 1
+  for filename in os.listdir(path):
+    output = f'{path}{output_name}{i}{output_file_ext}'
+    source = path + filename
+    os.rename(source, output)
+    i += 1
 
 if __name__ == '__main__':
    main()
