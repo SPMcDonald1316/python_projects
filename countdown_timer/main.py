8c36bf51ff45426dac0863c1260610a8c02898fa
sean <spmcdonald1316@gmail.com>
2024-02-16 16:06:08 -0600

    create simple timer

---
+++ countdown_timer/main.py
@@ -0,0 +1,15 @@
+import time
+
+def countdown(t):
+  while t:
+    mins, secs = divmod(t, 60)
+    timer = f'{mins:02d}:{secs:02d}'
+    print(timer, end='\r')
+    time.sleep(1)
+    t -= 1
+
+  print('Timer completed.')
+
+t = input("Enter time in seconds: ")
+
+countdown(int(t))
