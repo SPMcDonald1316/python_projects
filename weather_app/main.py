db2a0896304d8aca8aeadaecff79374396463d85
sean <spmcdonald1316@gmail.com>
2024-02-16 15:52:00 -0600

    fetch data and print

--- weather_app/main.py
+++ weather_app/main.py
@@ -6,4 +6,6 @@ city = input("Enter a city: ")
 
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={config.api_key}'
 
-weather_data = requests.get(url)
+weather_data = requests.get(url).json()
+
+pprint(weather_data)
