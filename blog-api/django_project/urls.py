d985a91762f36ec4596ac8cd13eaea109edfe400
sean <spmcdonald1316@gmail.com>
2025-03-22 14:24:25 -0500

    remove project

--- blog-api/django_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/", include("posts.urls")),
-    path("api-auth/", include("rest_framework.urls")),
-    path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
-]
