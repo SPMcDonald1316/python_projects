9b2c971098736a2c14b3b0768473bad54fab55e6
sean <spmcdonald1316@gmail.com>
2025-03-23 12:17:09 -0500

    test create, update, and delete views

--- blog-app/blog/tests.py
+++ blog-app/blog/tests.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -37,10 +38,39 @@ class BlogTests(TestCase):
         self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "A good title")
 
-    def test_detail_page(self):
+    def test_post_detailview(self):
         response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
         no_response = self.client.get("/post/100000/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertTemplateUsed(response, "post_detail.html")
         self.assertContains(response, "Nice body content")
+
+    def test_post_createview(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "new title",
+                "body": "new text",
+                "author": self.user.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "new title")
+        self.assertEqual(Post.objects.last().body, "new text")
+
+    def test_post_updateview(self):
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "updated title",
+                "body": "updated text",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "updated title")
+        self.assertEqual(Post.objects.last().body, "updated text")
+
+    def test_post_deleteview(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
