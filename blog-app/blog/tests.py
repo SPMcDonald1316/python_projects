c0ba11474fa1003bb35a0db09edf713c9b7c3862
sean <spmcdonald1316@gmail.com>
2025-03-23 11:07:37 -0500

    test home and detail pages

--- blog-app/blog/tests.py
+++ blog-app/blog/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 
 
@@ -21,3 +22,25 @@ class BlogTests(TestCase):
         self.assertEqual(self.post.author.username, "testuser")
         self.assertEqual(str(self.post), "A good title")
         self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+
+    def test_home_correct_location_at_url(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_correct_location_at_url(self):
+        response = self.client.get(f"/post/1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "A good title")
+
+    def test_detail_page(self):
+        response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
+        no_response = self.client.get("/post/100000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertTemplateUsed(response, "post_detail.html")
+        self.assertContains(response, "Nice body content")
