a97793eb23d8d5353e8f5bbc7e26821756e1d528
sean <spmcdonald1316@gmail.com>
2025-03-24 10:07:24 -0500

    test user creation

--- newspaper-app/accounts/tests.py
+++ newspaper-app/accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="testuser",
+            email="test@example.com",
+            password="testpass123",
+        )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "test@example.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="testsuperuser",
+            email="super@example.com",
+            password="testpass123",
+        )
+        self.assertEqual(admin_user.username, "testsuperuser")
+        self.assertEqual(admin_user.email, "super@example.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
