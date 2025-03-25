74615f27d7e6d4ddc9f645f53a94b152a45ef0f2
sean <spmcdonald1316@gmail.com>
2025-03-25 11:10:23 -0500

    fix minor bugs

--- newspaper-app/articles/urls.py
+++ newspaper-app/articles/urls.py
@@ -9,6 +9,6 @@ from .views import (
 urlpatterns = [
     path("", ArticleListView.as_view(), name="article_list"),
     path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
-    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_update"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
 ]
