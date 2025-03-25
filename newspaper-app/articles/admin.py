dac3efbeed0a69b3772b1aaad8ddf7684220ba7a
sean <spmcdonald1316@gmail.com>
2025-03-25 10:30:54 -0500

    add articles to admin page, adjust list display

--- newspaper-app/articles/admin.py
+++ newspaper-app/articles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "body",
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
