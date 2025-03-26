5f19286982ef10560465c4751292644f11a2ee5b
sean <spmcdonald1316@gmail.com>
2025-03-26 11:46:53 -0500

    test book model, url, template, and view

--- library-api/books/tests.py
+++ library-api/books/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="Book Title",
+            subtitle="Book Subtitle",
+            author="Book Author",
+            isbn="Book ISBN",
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "Book Title")
+        self.assertEqual(self.book.subtitle, "Book Subtitle")
+        self.assertEqual(self.book.author, "Book Author")
+        self.assertEqual(self.book.isbn, "Book ISBN")
+
+    def test_book_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "books/book_list.html")
+        self.assertContains(response, "Book Title")
