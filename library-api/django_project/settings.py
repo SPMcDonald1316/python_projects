6d216c56428078de5edaec60ef2db3334b20f540
sean <spmcdonald1316@gmail.com>
2025-03-27 18:21:34 -0400

    prepare for production, project commit

--- library-api/django_project/settings.py
+++ library-api/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4$oy6-*mdtssu_h(i)@4=604*cn$2p!u$*z5(hjcm^$y3)3&)-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
