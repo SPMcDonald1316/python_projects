a898676b64c9c412e2b1787f7635054cd530022e
sean <spmcdonald1316@gmail.com>
2025-03-27 19:58:21 -0400

    test api endpoints

--- todo-api/todos/tests.py
+++ todo-api/todos/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
 from .models import Todo
 
@@ -15,3 +17,17 @@ class TodoModelTest(TestCase):
         self.assertEqual(self.todo.title, "a todo")
         self.assertEqual(self.todo.body, "doing something")
         self.assertEqual(str(self.todo), "a todo")
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+
+    def test_api_detailview(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}), format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "a todo")
