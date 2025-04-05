22606cab07a2e5d9e8891c99a5b6fa512c847dac
sean <spmcdonald1316@gmail.com>
2025-04-05 14:27:29 -0500

    add allauth, config settings

--- bookstore/django_project/settings.py
+++ bookstore/django_project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     # Third Party
     "crispy_forms",
     "crispy_bootstrap5",
+    "allauth",
+    "allauth.account",
     # Local
     "accounts",
     "pages",
@@ -138,10 +140,16 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Custom User Model
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-# Specify Redirects
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
-
 # django-crispy-forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# django-allauth config
+LOGIN_REDIRECT_URL = "home"
+ACCOUNT_LOGOUT_REDIRECT = "home"
+SITE_ID = 1
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
