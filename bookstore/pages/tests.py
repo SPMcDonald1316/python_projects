3c8b4aca0bc54254bc9cfa7b93b5e98223e7b56b
sean <spmcdonald1316@gmail.com>
2025-04-05 13:21:09 -0500

    test about page

--- bookstore/pages/tests.py
+++ bookstore/pages/tests.py
@@ -1,9 +1,10 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
-class HomepageTests(SimpleTestCase):
+
+class HomePageTests(SimpleTestCase):
     def setUp(self):
         url = reverse("home")
         self.response = self.client.get(url)
@@ -17,3 +18,19 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "about.html")
+        self.assertContains(self.response, "About Page")
+        self.assertNotContains(self.response, "Home")
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
